f2f78dcfa61a073240de25486f24968460bb3ca6
pramodpk03 <104554246+pramodpk03@users.noreply.github.com>
2022-04-29 16:40:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,19 +41,7 @@ def prediction(data,sampling_rate):
     st.markdown(original_title, unsafe_allow_html=True)
     st.subheader(detected_emotion[0].upper())
 
-def prediction_mlp(data,sampling_rate):
-    dict_values = {0:"neutral",1:"calm",2:"happy",3:"sad",4:"angry",5:"fear",6:"disgust",7:"pleasant surprise"}
-    path = "MLP_model.pkl"
-    
-    MLP_model = joblib.load(open(path,"rb"))
-    arr=[] 
-    mfccs = np.mean(librosa.feature.mfcc(y=data, sr=sampling_rate, n_mfcc=40).T,axis=0) 
-    arr.append(mfccs)
-    predict = MLP_model.predict(arr)
-    detected_emotion = [dict_values[val] for val in predict]
-    original_title = '<p style="font-family:Courier; color:DarkSeaGreen; font-size: 18px;"><b>EMOTION DETECTED</b></p>'
-    st.markdown(original_title, unsafe_allow_html=True)
-    st.subheader(detected_emotion[0].upper())    
+   
     
 
 def main():
@@ -87,43 +75,20 @@ def main():
                     st.write("\n")
                     st.write("\n")
                     prediction(data,sampling_rate) 
-    elif choice == "MLP Model":
-        with col1:
-            original_title = '<p style="font-family:Courier; color:DarkSeaGreen; font-size: 20px;"><b>CHOOSE AUDIO FILE</b></p>'
-            st.markdown(original_title, unsafe_allow_html=True)
-            audio_file = st.file_uploader("", type=['WAV', 'MP3', 'ogg'])
-            if audio_file is not None:
-                with col1:
-                    data, sampling_rate = librosa.load(audio_file)
-                    original_title = '<p style="font-family:Courier; color:DarkSeaGreen; font-size: 18px;"><b>WAVE FORM</b> </p>'
-                    st.markdown(original_title, unsafe_allow_html=True)
-                    data = wave_plot(data,sampling_rate)
-                with col2 :
-                    original_title = '<p style="font-family:Courier; color:DarkSeaGreen; font-size: 18px;"><b>PLAY AUDIO</b> </p>'
-                    st.markdown(original_title, unsafe_allow_html=True)
-                    st.write('\n')
-                    st.write("\n")
-                    st.write("\n")
-                    st.audio(audio_file, format='audio/WAV', start_time=0)
-                    st.write("\n")
-                    st.write("\n")
-                    st.write("\n")
-                    st.write("\n")
-                    st.write("\n")
-                    prediction_mlp(data,sampling_rate) 
+            
     else:
         
-        original_title = '<p style="font-family:Courier; color:Teal; font-size: 18px;"><b>Speech Emotion Recognition (SER) can be defined as extraction of the emotional state of the speaker from his or her speech signal.We will be using different models to detect emotions for the audio input provided by the user.</b></p>'
-        st.markdown(original_title, unsafe_allow_html=True) 
-        original_title = '<p style="font-family:Courier; color:white;font-size: 18px;"></br><b>User will have two options for trying out : MLP, CNN<b><p>'
-        st.markdown(original_title, unsafe_allow_html=True)
-        original_title = '<br></br>'
-        st.markdown(original_title, unsafe_allow_html=True) 
-        original_title = '<p style="font-family:Courier; color:Teal; font-size: 25px;"><b>CREATED BY</b></p>'
-        st.markdown(original_title, unsafe_allow_html=True)  
-        st.write("\n")
-        original_title = '<p style="font-family:Courier; color:Teal; font-size: 20px;"><b>SAKSHI DHYANI</b></p>'
-        st.markdown(original_title, unsafe_allow_html=True)       
+            original_title = '<p style="font-family:Courier; color:Teal; font-size: 18px;"><b>Speech Emotion Recognition (SER) can be defined as extraction of the emotional state of the speaker from his or her speech signal.We will be using different models to detect emotions for the audio input provided by the user.</b></p>'
+            st.markdown(original_title, unsafe_allow_html=True) 
+            original_title = '<p style="font-family:Courier; color:white;font-size: 18px;"></br><b>User will have two options for trying out : MLP, CNN<b><p>'
+            st.markdown(original_title, unsafe_allow_html=True)
+            original_title = '<br></br>'
+            st.markdown(original_title, unsafe_allow_html=True) 
+            original_title = '<p style="font-family:Courier; color:Teal; font-size: 25px;"><b>CREATED BY</b></p>'
+            st.markdown(original_title, unsafe_allow_html=True)  
+            st.write("\n")
+            original_title = '<p style="font-family:Courier; color:Teal; font-size: 20px;"><b>SAKSHI DHYANI</b></p>'
+            st.markdown(original_title, unsafe_allow_html=True)       
 
 
 if __name__ == '__main__':
